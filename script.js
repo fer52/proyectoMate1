9bd790b3c69865d33e833aa024d6bd887c4da47e
fcanu <fcanu@plusti.com>
2023-10-28 08:04:35 -0600

    Ajustes para función math1

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 //agregar código para su función
-function math1 (){
 
+//Función para crear un grafo FC
+function math1 (){
+    // create an array with nodes
+    var nodes = new vis.DataSet([
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+    ]);
+
+    // create an array with edges
+    var edges = new vis.DataSet([
+        {from: 1, to: 3},
+        {from: 1, to: 2},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+    ]);
+
+    // create a network
+    var container = document.getElementById('mynetwork');
+
+    // provide the data in the vis format
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+    var options = {};
+
+    // initialize your network!
+    var network = new vis.Network(container, data, options);
+    
 }
 function math2 (){
-    
+    console.log(2);
 }
 function math3 (){
-    
+    console.log(3);
 }
 function math4 (){
-    
+    console.log(4);
 }
 function math5 (){
-    
+    console.log(5);
 }
 
 function changeContent(sectionId) {
     const sections = document.querySelectorAll('.page-content');
-    sections.forEach((section) => {
-        section.style.display = 'none';
-    });
-    document.getElementById(sectionId).style.display = 'block';
+    // sections.forEach((section) => {
+    //     section.style.display = 'none';
+    // });
+    // document.getElementById(sectionId).style.display = 'block';
 
     switch (sectionId){
         case "section1":
@@ -42,7 +73,7 @@ function changeContent(sectionId) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const navigationLinks = document.querySelectorAll('.mdl-navigation__link');
+    const navigationLinks = document.querySelectorAll('.mdl-tabs__tab');
     navigationLinks.forEach((link) => {
         link.addEventListener('click', (e) => {
             const sectionId = e.target.getAttribute('href').substring(1);
@@ -51,7 +82,17 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Mostrar la primera sección por defecto
-    changeContent('section1');
+    // changeContent('section1');
+
+
+    //agrega evento a boton para crear nodo
+    let btnNode = document.querySelector('#newNode');;
+    if (btnNode != null && typeof(btnNode)!='undefined' && btnNode!=""){
+        btnNode.addEventListener('click', (e) => {
+           math1(); 
+        });
+    }
+        
 });
 
 
